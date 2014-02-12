795bde9ff9949aa820daa1c969757c2e1e951f83
dexX7 <dexx@bitwatch.co>
2014-02-12 10:50:53 +0100

    Add Mastercoin as currency
    Added Mastercoin, Mastercoin icon, lookup for Mastercoin balances and
    transaction count via four web APIs, option "send_button [true; false]",
    auto reset box-sizing.

--- README.md
+++ README.md
@@ -1,49 +1,64 @@
-coinwidget.com
+CoinWidget.com (msc-mod)
 ==============
 
-*The Bitcoin and Litecoin Donation Button*
+*The Bitcoin, Litecoin and Mastercoin Donation Button*
 
 Visit http://coinwidget.com/ for full documentation, demo, and a link code wizard.
 
-CoinWidget was created by http://scotty.cc/ (hire me for your next project!)
+CoinWidget was initially created by http://scotty.cc/.
+
+Improvements were made by [q--](https://github.com/q--/coinwidget.com).
+
+This version ("msc-mod") adds the currency Mastercoin and uses one of the following data providers:
+
+	https://masterchest.info/
+	http://www.mymastercoins.com/
+	https://masterchain.info/
+	http://mastercoin-explorer.com/
 
 Released under the Open Source **MIT License** (see **LICENSE** file for details).
-
-Please help keep this project alive! Tell someone about this widget! 
-
-Donations are welcome and will go towards further development of this project as well as other crypto related projects. Use the wallet addresses below to donate. 
-
-	BTC: 122MeuyZpYz4GSHNrF98e6dnQCXZfHJeGS
-	LTC: LY1L6M6yG26b4sRkLv4BbkmHhPn8GR5fFm
-
-*Thank you for your support and generosity!*
 
 
 Installation
 ==============
-A. Grab the latest version from GitHub: https://github.com/scottycc/coinwidget.com
+A. Download all files from GitHub: https://github.com/dexX7/coinwidget.com
 
-B. Open **widget/coin.js** and find:
+B. Open **coin.js** and find:
 
-	source: 'http://coinwidget.com/widget/'
+	source: 'http://mastercoin-faucet.com/tips/'
 
 C. Change the URL portion of this line to your own server/path.
 
-D. (optional) Open **lookup.php** and consider implementing a caching method based on your own style and preference.
+D. (optional) Find the following line and replace CSS file:
+
+	href   : CoinWidgetCom.source+'coin.css'
+  
+E. (optional) Find the following line and replace JQuery file:
+
+	source    : $prefix + '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'
+  
+F. (optional) Open **lookup.php** and change data provider. This can be easily done by uncommenting a few lines of code.
+
+G. (optional) Create your own implementation of lookup.php, stop using web APIs to fetch data or add more currencies.
+
+G. Upload files to your server.
+
+H. Follow the example code to include the donation button.
+
+Watch out for **call to action** comments in the source code.
 
 
-Example Code
+Example code and usage
 ==============
 
 See the file: **code-sample.html**
 
-The complete list of the options and acceptable values can be found on http://coinwidget.com/.
+A list of available options and acceptable values can be found on http://coinwidget.com/.
 
-You can also use the wizard on http://coinwidget.com/ to generate linking codes, just be sure to change: `<script src="http://coinwidget.com/widget/coin.js"></script>` to use your own hosted copy of coin.js.
-
-*Enjoy! -scotty*
+Note: the option **send_button** is new and allows to hide the "click here to open wallet" button.
 
 
 ==============
-*If you run into a bug, or a styling issue, or if you need help or have an idea please e-mail:
-coinwidget.com@gmail.com*
+*If you run into a bug, or if you need help or have an idea please e-mail:
+coinwidget.com@gmail.com (original author)
+dexx@bitwatch.co (msc-mod)*
--- lookup.php
+++ lookup.php
@@ -3,14 +3,16 @@
 /** -
 
 Donations welcome:
-	BTC: 122MeuyZpYz4GSHNrF98e6dnQCXZfHJeGS
-	LTC: LY1L6M6yG26b4sRkLv4BbkmHhPn8GR5fFm
-		~ Thank you!
+  BTC: 122MeuyZpYz4GSHNrF98e6dnQCXZfHJeGS
+  LTC: LY1L6M6yG26b4sRkLv4BbkmHhPn8GR5fFm
+  MSC: 1JCwo8qnjDquGnx44N81NNehqytksL92zM
+    ~ Thank you!
 
 MIT License (MIT)
 
 Copyright (c) 2013 http://coinwidget.com/ 
 Copyright (c) 2013 http://scotty.cc/
+Copyright (c) 2014 http://bitwatch.co/
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -32,78 +34,156 @@ THE SOFTWARE.
 
 */
 
-	header("Content-type: text/javascript");
-	/*
-		you should server side cache this response, especially if your site is active
-	*/
-	$data = isset($_GET['data'])?$_GET['data']:'';
-	if (!empty($data)) {
-		$data = explode("|", $data);
-		$responses = array();
-		if (!empty($data)) {
-			foreach ($data as $key) {
-				list($instance,$currency,$address) = explode('_',$key);
-				switch ($currency) {
-					case 'bitcoin': 
-						$response = get_bitcoin($address);
-						break;
-					case 'litecoin': 
-						$response = get_litecoin($address);
-						break;
-				}
-				$responses[$instance] = $response;
-			}
-		}
-		echo 'var COINWIDGETCOM_DATA = '.json_encode($responses).';';
-	}
+  /**
+  * Optional call to action:
+  * Set cache/expiration headers.
+  */
+  error_reporting(0);
+  header("Content-type: text/javascript");
+  /**
+  * You should server side cache this response, especially if your site is active.
+  */
+  $data = isset($_GET['data'])?$_GET['data']:'';
+  if (!empty($data)) {
+    $data = explode("|", $data);
+    $responses = array();
+    if (!empty($data)) {
+      foreach ($data as $key) {
+        list($instance,$currency,$address) = explode('_',$key);
+        switch ($currency) {
+          case 'bitcoin': 
+            $response = get_bitcoin($address);
+            break;
+          case 'litecoin': 
+            $response = get_litecoin($address);
+            break;
+          case 'mastercoin': 
+            $response = get_mastercoin($address);
+            break;
+          /**
+           * Optional call to action:
+           * Add other currencies.
+           */
+        }
+        $responses[$instance] = $response;
+      }
+    }
+    echo 'var COINWIDGETCOM_DATA = '.json_encode($responses).';';
+  }
 
-	function get_bitcoin($address) {
-		$return = array();
-		$data = get_request('http://blockchain.info/address/'.$address.'?format=json&limit=0');
-		if (!empty($data)) {
-			$data = json_decode($data);
-			$return += array(
-				'count' => (int) $data->n_tx,
-				'amount' => (float) $data->total_received/100000000
-			);
-			return $return;
-		}
-	}
+  function get_bitcoin($address) {
+    $return = array();
+    $data = get_request('http://blockchain.info/address/'.$address.'?format=json&limit=0');
+    if (!empty($data)) {
+      $data = json_decode($data);
+      $return += array(
+        'count' => (int) $data->n_tx,
+        'amount' => (float) $data->total_received/100000000
+      );
+      return $return;
+    }
+  }
 
-	function get_litecoin($address) {
-		$return = array();
-		$data = get_request('http://explorer.litecoin.net/address/'.$address);
-		if (!empty($data) 
-		  && strstr($data, 'Transactions in: ') 
-		  && strstr($data, 'Received: ')) {
-		  	$return += array(
-				'count' => (int) parse($data,'Transactions in: ','<br />'),
-				'amount' => (float) parse($data,'Received: ','<br />')
-			);
-		  	return $return;
-		}
-	}
+  function get_litecoin($address) {
+    $return = array();
+    $data = get_request('http://explorer.litecoin.net/address/'.$address);
+    if (!empty($data) 
+      && strstr($data, 'Transactions in: ') 
+      && strstr($data, 'Received: ')) {
+        $return += array(
+        'count' => (int) parse($data,'Transactions in: ','<br />'),
+        'amount' => (float) parse($data,'Received: ','<br />')
+      );
+        return $return;
+    }
+  }
+  
+  function get_mastercoin($address, $currencyid=1) {
+    $return = array();
+    $return += array(
+        'count' => get_msc_tx_count($address),
+        'amount' => get_msc_balance($address)
+      );
+    return $return;
+  }
 
-	function get_request($url,$timeout=4) {
-		if (function_exists('curl_version')) {
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout);
-			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13');
-			$return = curl_exec($curl);
-			curl_close($curl);
-			return $return;
-		} else {
-			return @file_get_contents($url);
-		}
-	}
+  function get_request($url,$timeout=4) {
+    if (function_exists('curl_version')) {
+      $curl = curl_init();
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout);
+      curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13');
+      $return = curl_exec($curl);
+      curl_close($curl);
+      return $return;
+    } else {
+      return @file_get_contents($url);
+    }
+  }
 
-	function parse($string,$start,$stop) {
-		if (!strstr($string, $start)) return;
-		if (!strstr($string, $stop)) return;
-		$string = substr($string, strpos($string,$start)+strlen($start));
-		$string = substr($string, 0, strpos($string,$stop));
-		return $string;
-	}
+  function parse($string,$start,$stop) {
+    if (!strstr($string, $start)) return;
+    if (!strstr($string, $stop)) return;
+    $string = substr($string, strpos($string,$start)+strlen($start));
+    $string = substr($string, 0, strpos($string,$stop));
+    return $string;
+  }
+  
+  function get_msc_balance($address, $currencyid=1)
+  {
+    /**
+     * Optional call to action:
+     * Replace data provider
+     */
+    
+    $data_provider_url =
+      'https://masterchest.info/mastercoin_verify/addresses.aspx';
+        
+    // $data_provider_url = 
+    //   'http://www.mymastercoins.com/jaddress.aspx?address='.$address.'&currency_id='.$currencyid;
+
+    // $data_provider_url = 
+    //   'https://masterchain.info/mastercoin_verify/addresses/'.($currencyid-1);
+    
+    // $data_provider_url = 
+    //   'http://mastercoin-explorer.com/mastercoin_verify/addresses?currency_id='.$currencyid;
+    
+    $data = get_request($data_provider_url);
+    $json = json_decode($data);
+    foreach($json as $entry)
+    {
+      if($entry->address == $address)
+      {
+        return (float) $entry->balance;
+      }
+    }
+    return 0.0;
+  }
+  
+  function get_msc_tx_count($address, $currencyid=1)
+  {
+    /**
+     * Optional call to action:
+     * Replace data provider
+     */
+     
+    $data_provider_url = 
+      'https://masterchest.info/mastercoin_verify/transactions.aspx?address='.$address.'&currencyid='.$currencyid;
+     
+    // $data_provider_url = 
+    //   'http://www.mymastercoins.com/jtransactions.aspx?Address='.$address.'&currency_id='.$currencyid;
+      
+    // $data_provider_url = 
+    //   'https://masterchain.info/mastercoin_verify/transactions/'.$address;
+      
+    // $data_provider_url = 
+    //   'http://mastercoin-explorer.com/mastercoin_verify/transactions/'.$address.'&currency_id='.$currencyid;
+      
+    $data = get_request($data_provider_url);
+    $json = json_decode($data);
+    return count($json->transactions);
+  }
+  
+?>
